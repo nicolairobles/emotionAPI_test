c80313736a1b18d2c22517a00b4fee7ca3428f86
Nicolai Robles <nicolai.robles@gmail.com>
2016-05-10 14:37:15 -0400

    edits

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,5 @@
 class Video < ActiveRecord::Base
+	
 	has_attached_file :video_file
 	validates_attachment :video_file, content_type: { content_type: ["video/mp4", "video/mov", "video/avi", "video/wmv", "video/mpg", "video/webm"] }
 
