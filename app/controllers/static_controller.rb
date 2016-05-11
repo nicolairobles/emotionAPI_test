2b289f3c752b96dadc53c589de5e4ca87138be3c
Nicolai Robles <nicolai.robles@gmail.com>
2016-05-11 14:38:45 -0400

    reached API for each image and stored data in variables

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,29 +2,54 @@ class StaticController < ApplicationController
 require 'net/http'
 
 	def index
-		# movie = FFMPEG::Movie.new("/Users/nicolai/code/projects/emotionAPI_test/public/mark_zuck.mp4")
-		# movie.transcode("movie.mp4", "-r 1 /Users/nicolai/code/projects/emotionAPI_test/public/test2/image-%04d.jpeg") { |progress| puts progress } # 0.2 ... 0.5 ... 1.0
+		# Create new FFMPEG Movie
+		movie = FFMPEG::Movie.new("/Users/nicolai/code/projects/emotionAPI_test/public/mark_zuck.mp4")
+		# Parse FFMPEG Movie into frames
+		movie.transcode("movie.mp4", "-r 1 /Users/nicolai/code/projects/emotionAPI_test/public/test2/image-%04d.jpeg") { |progress| puts progress } # 0.2 ... 0.5 ... 1.0
 
-		# Emotion Image API attempt
-		# 	uri = URI('https://api.projectoxford.ai/emotion/v1.0/recognize')
-		# 	uri.query = URI.encode_www_form({
-		# 	})
+		# Send Frames to Amazon for storage
 
-		# 	data = File.read("./public/full color pic.png")
+		# Grab frames from Amazon to send them to API
 
-		# 	request = Net::HTTP::Post.new(uri.request_uri)
-		# 	# Request headers
-		# 	request['Ocp-Apim-Subscription-Key'] = '6f027d66bef64938872d4f519168495f'
-		# 	# Request body
-		# 	# request.body = data
-		# 	request['Content-Type'] = 'application/octet-stream'
-		# 	request.body = data
-		# 	# '{"url": "http://localhost:4000/mark_zuck.mp4"}'
-		# 	response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
-		# 	    http.request(request)
-		# 	end
+		# Send frames to API
+		file = File.read(File.join(Rails.root,'public', 'test2','image-0001.jpeg'))
+		directory = Dir[File.join(Rails.root,'public', 'test2',"*")]
+		directory.each {|image| puts "I got #{image}" }
 
-		# puts response.body
+		# Loop through directory where images are stored and read each file
+		directory.each do |image|
+		# Emotion Image API call
+			# Set parameters
+			uri = URI('https://api.projectoxford.ai/emotion/v1.0/recognize')
+			uri.query = URI.encode_www_form({
+			})
+			request = Net::HTTP::Post.new(uri.request_uri)
+			request['Ocp-Apim-Subscription-Key'] = ENV["MEA_SubscriptionKey1"]
+			request['Content-Type'] = 'application/octet-stream'
+			# Read in appropriate file
+			request.body = File.read("#{image}")
+			# Make HTTP request to API
+			response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+			    http.request(request)
+			end
+			# Pull out JSON data from response
+			puts response.body
+			# Inject data into Frames table
+			json_scores = JSON.parse(response.body).first
+			anger = json_scores["anger"]
+			contempt = json_scores["contempt"]
+			disgust = json_scores["disgust"]
+			fear = json_scores["fear"]
+			happiness = json_scores["happiness"]
+			neutral = json_scores["neutral"]
+			sadness = json_scores["sadness"]
+			surprise = json_scores["surprise"]
+			
+			
+		end
+
+
+
 
 
 
@@ -47,9 +72,9 @@ require 'net/http'
 	# 	    http.request(request)
 	# 	end
 	# 	p "request sent"
-	# 	response.each_header do |key, value|
-	# 	  p "#{key} => #{value}"
-	# 	end
+		# response.each_header do |key, value|
+		#   p "#{key} => #{value}"
+		# end
 	# 	p "response received"
 	# 	operation_location = response["operation-location"]
 	# 	oid = operation_location.split("/")[6]
@@ -72,11 +97,11 @@ require 'net/http'
 	# 	response.each_header do |key, value|
 	# 	  p "#{key} => #{value}"
 	# 	end
-		
+
 	# 	p response.body
-	# 	# ENV['MEA_SubscriptionKey1']
-	# 	# Test w/ Curl
-	# 	# curl -v -X GET "https://api.projectoxford.ai/emotion/v1.0/operations/b055e518-bb28-4a3b-be3b-8445dce5232d" -H "Ocp-Apim-Subscription-Key: fd89d512b5c345228039170679f1c097"
+		# ENV['MEA_SubscriptionKey1']
+		# Test w/ Curl
+		# curl -v -X GET "https://api.projectoxford.ai/emotion/v1.0/operations/b055e518-bb28-4a3b-be3b-8445dce5232d" -H "Ocp-Apim-Subscription-Key: fd89d512b5c345228039170679f1c097"
 
 	end 
 	
